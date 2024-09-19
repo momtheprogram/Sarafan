ff6e74c28e063d5fa0e13a457c46c5024dfa6ce3
momtheprogram <momtheprogram@gmail.com>
2024-09-19 10:57:40 +0300

    settings, adminka

--- grosery_store/products/admin.py
+++ grosery_store/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Subcategory, Product
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Product)
